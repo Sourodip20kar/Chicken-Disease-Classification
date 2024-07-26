897f7ea39d4991e7632d8fa3c50de4543c6785cd
Sourodip Kar <51.sourodipkar2002n.i.t@gmail.com>
2024-07-26 20:50:39 +0530

    folder structed created

--- template.py
+++ template.py
@@ -23,3 +23,19 @@ list_of_files=[
     "setup.py",
     "research/classification.ipynb"
 ]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename =os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as file:
+            pass
+            logging.info(f"Created file: {filepath}")
+    else:
+        logging.info(f"File already exists: {filepath}")
